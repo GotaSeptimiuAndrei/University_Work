2c2cdc52c243d3c37f4d11b6c8643d70355601cf
Goța Septimiu Andrei <andreigota2003@gmail.com>
2024-06-29 22:21:15 +0300

    .

---
+++ Semester 4/Software Engineering/UBB-SE-2024-Business/ISS-Frontend/Data/ISS_FrontendContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ISS_Frontend.Models;
+using ISS_Frontend.Entity;
+
+namespace ISS_Frontend.Data
+{
+    public class ISS_FrontendContext : DbContext
+    {
+        public ISS_FrontendContext (DbContextOptions<ISS_FrontendContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ISS_Frontend.Models.BankAccount> BankAccount { get; set; } = default!;
+        public DbSet<ISS_Frontend.Entity.Product> Product { get; set; } = default!;
+        public DbSet<ISS_Frontend.Entity.AdAccount> AdAccount { get; set; } = default!;
+        public DbSet<ISS_Frontend.Entity.Ad> Ad { get; set; } = default!;
+    }
+}
